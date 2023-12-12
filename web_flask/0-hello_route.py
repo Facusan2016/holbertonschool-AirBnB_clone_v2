8bea77d26e7334a7f6afcf0528906df15edda9df
Facusan2016 <facusan2016@gmail.com>
2023-12-12 10:11:33 -0300

    added exercise 0 and readme

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Main program for Flask application.
+"""
+if __name__ == "__main__":
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route("/")
+    def helloBnb():
+        """
+            Function to retun "Hello HBNB!"
+        """
+        return "Hello HBNB!"
+
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
