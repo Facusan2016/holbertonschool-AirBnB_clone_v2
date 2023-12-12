89c4949fb24549a3c6660147e48b4dd9142ca14c
Facusan2016 <facusan2016@gmail.com>
2023-12-12 10:47:11 -0300

    added exercise 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Main program for Flask application.
+"""
+if __name__ == "__main__":
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route("/")
+    def helloBnb():
+        """
+            Function to retun "Hello HBNB!".
+        """
+        return "Hello HBNB!"
+
+    @app.route("/hbnb")
+    def hbnb():
+        """
+            Function to retun "HBNB".
+        """
+        return "HBNB"
+
+    @app.route("/c/<text>")
+    def cName(text:str):
+        """
+            Function to return the text variable with a C.
+        """
+        formatted_text = text.replace('_', ' ')
+        return f'C {formatted_text}'
+
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
