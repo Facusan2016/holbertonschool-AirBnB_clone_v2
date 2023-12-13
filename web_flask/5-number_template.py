406b87cb7053cdf44355dbcf138190d85ecad999
Facusan2016 <facusan2016@gmail.com>
2023-12-12 21:05:14 -0300

    added exercise 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+    Main program for Flask application.
+"""
+if __name__ == "__main__":
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route("/")
+    def helloBnb():
+        """
+            Function to retun "Hello HBNB!".
+        """
+        return "Hello HBNB!"
+
+    @app.route("/hbnb")
+    def hbnb():
+        """
+            Function to retun "HBNB".
+        """
+        return "HBNB"
+
+    @app.route("/c/<text>")
+    def cName(text):
+        """
+            Function to return the text variable with a C.
+        """
+        formatted_text = text.replace('_', ' ')
+        return f'C {formatted_text}'
+
+    @app.route('/python/', defaults={'text': 'is_cool'})
+    @app.route("/python/<text>")
+    def PythonName(text):
+        """
+            Function to return the text variable with a C.
+        """
+
+        formatted_text = text.replace('_', ' ')
+        return f'Python {formatted_text}'
+
+    @app.route("/number/<int:n>")
+    def routeNum(n):
+        """
+            Route that returns the number if it's an integer
+        """
+        return f"{n} is a number"
+
+    @app.route("/number_template/<int:n>")
+    def routeNumTemplate(n):
+        """
+            Route that returns a html if n is an integer
+        """
+        from flask import render_template
+        return render_template('5-number.html')
+
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
